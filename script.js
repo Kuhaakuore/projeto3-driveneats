19b16f1d35cfcc8e6e60a36fd133e788820a262c
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-04-03 03:46:42 -0300

    Projeto completo pré-avaliação no HUB

--- script.js
+++ script.js
@@ -1,119 +1,86 @@
-let selected_meal = '';
-let selected_drink = '';
-let selected_dessert = '';
-let meal_price = 0;
-let drink_price = 0;
-let dessert_price = 0;
+let meal = '';
+let drink = '';
+let dessert = '';
+let mealPrice = 0;
+let drinkPrice = 0;
+let dessertPrice = 0;
 let total = 0;
-let items_picked = 0;
+let clientName = '';
+let clientAddress = '';
 
-const meals = document.getElementsByClassName("meal");
-const drinks = document.getElementsByClassName("drink");
-const desserts = document.getElementsByClassName("dessert");
-const order_button = document.getElementById("order-button");
 
-function selectMeal(item) {
-    if (item.classList.contains("selected")) {
-        item.classList.toggle("selected");
-        if (items_picked > 0) {
-            items_picked--;
-        }
-        if (order_button.classList.contains("close-order")) {
-            order_button.classList.remove("close-order");
-            order_button.innerHTML = "Selecione os 3 itens <br />para fechar o pedido";
-            order_button.disabled = true;
-        }
-    }
-    else {
-        Array.from(meals).forEach(meal => {
-            if (meal.classList.contains("selected")) {
-                meal.classList.toggle("selected");
-                items_picked--;
+function validateOrder() {
+    if (meal !== '') {
+        if (drink !== '') {
+            if (dessert !== '') {
+                const order_button = document.querySelector(".order-button");
+                order_button.classList.add("close-order");
+                order_button.disabled = false;
+                order_button.innerHTML = "Fechar pedido"
+                total = mealPrice + drinkPrice + dessertPrice;
             }
-        });
-        item.classList.toggle("selected");
-        selected_meal = item.querySelector("h1").innerHTML;
-        value_with_comma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
-        meal_price = parseFloat(value_with_comma.replace(',', '.'));
-        if (items_picked <= 2) {
-            items_picked++;
-        }
-        if (items_picked === 3) {
-            order_button.classList.add("close-order");
-            order_button.disabled = false;
-            order_button.innerHTML = "Fechar pedido"
-            total = meal_price + drink_price + dessert_price;
         }
     }
 }
 
-function selectDrink(item) {
-    if (item.classList.contains("selected")) {
-        item.classList.toggle("selected");
-        if (items_picked > 0) {
-            items_picked--;
+function selectItem(item) {
+    if (item.classList.contains("meal")) {
+        const previousSelectedItem = document.querySelector(".meal.selected");
+        console.log(previousSelectedItem);
+        if (previousSelectedItem !== null) {
+            previousSelectedItem.classList.remove("selected");
         }
-        if (order_button.classList.contains("close-order")) {
-            order_button.classList.remove("close-order");
-            order_button.innerHTML = "Selecione os 3 itens <br />para fechar o pedido";
-            order_button.disabled = true;
+        item.classList.add("selected");
+        meal = item.querySelector("h1").innerHTML;
+        valueWithComma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
+        mealPrice = parseFloat(valueWithComma.replace(',', '.'));
+    }
+    else if (item.classList.contains("drink")) {
+        const previousSelectedItem = document.querySelector(".drink.selected");
+        if (previousSelectedItem !== null) {
+            previousSelectedItem.classList.remove("selected");
         }
+        item.classList.add("selected");
+        drink = item.querySelector("h1").innerHTML;
+        valueWithComma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
+        drinkPrice = parseFloat(valueWithComma.replace(',', '.'));
     }
     else {
-        Array.from(drinks).forEach(meal => {
-            if (meal.classList.contains("selected")) {
-                meal.classList.toggle("selected");
-                items_picked--;
-            }
-        });
-        item.classList.toggle("selected");
-        selected_drink = item.querySelector("h1").innerHTML;
-        value_with_comma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
-        drink_price = parseFloat(value_with_comma.replace(',', '.'));
-        if (items_picked <= 2) {
-            items_picked++;
+        const previousSelectedItem = document.querySelector(".dessert.selected");
+        if (previousSelectedItem !== null) {
+            previousSelectedItem.classList.remove("selected");
         }
-        if (items_picked === 3) {
-            order_button.classList.add("close-order");
-            order_button.disabled = false;
-            order_button.innerHTML = "Fechar pedido"
-            total = meal_price + drink_price + dessert_price;
-        }
+        item.classList.add("selected");
+        dessert = item.querySelector("h1").innerHTML;
+        valueWithComma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
+        dessertPrice = parseFloat(valueWithComma.replace(',', '.'));
     }
+    validateOrder();
 }
 
-function selectDessert(item) {
-    if (item.classList.contains("selected")) {
-        item.classList.toggle("selected");
-        if (items_picked > 0) {
-            items_picked--;
+function closeOrder() {
+    clientName = prompt("Por favor informe o seu nome");
+    clientAddress = prompt("Muito obrigado, agora, por favor informe o endereço de entrega");
+    const informationContainer = document.querySelector(".information-container");
+    informationContainer.querySelector(".order-meal .name").innerHTML = meal;
+    informationContainer.querySelector(".order-drink .name").innerHTML = drink;
+    informationContainer.querySelector(".order-dessert .name").innerHTML = dessert;
+    informationContainer.querySelector(".order-meal .price").innerHTML = `R$ ${mealPrice.toFixed(2).toString().replace('.', ',')}`;
+    informationContainer.querySelector(".order-drink .price").innerHTML = `R$ ${drinkPrice.toFixed(2).toString().replace('.', ',')}`;
+    informationContainer.querySelector(".order-dessert .price").innerHTML = `R$ ${dessertPrice.toFixed(2).toString().replace('.', ',')}`;
+    informationContainer.querySelector(".order-value .price").innerHTML = `R$ ${total.toFixed(2).toString().replace('.', ',')}`;
+    const confirmationScreen = document.querySelector(".confirmation-screen");
+    confirmationScreen.classList.add("activated");
+}
 
-        }
-        if (order_button.classList.contains("close-order")) {
-            order_button.classList.remove("close-order");
-            order_button.innerHTML = "Selecione os 3 itens <br />para fechar o pedido";
-            order_button.disabled = true;
-        }
-    }
-    else {
-        Array.from(desserts).forEach(meal => {
-            if (meal.classList.contains("selected")) {
-                meal.classList.toggle("selected");
-                items_picked--;
-            }
-        });
-        item.classList.toggle("selected");
-        selected_dessert = item.querySelector("h1").innerHTML;
-        value_with_comma = item.querySelector("h3").innerHTML.replace(/[^\d\,]*/g, '');
-        dessert_price = parseFloat(value_with_comma.replace(',', '.'));
-        if (items_picked <= 2) {
-            items_picked++;
-        }
-        if (items_picked === 3) {
-            order_button.classList.add("close-order");
-            order_button.disabled = false;
-            order_button.innerHTML = "Fechar pedido"
-            total = meal_price + drink_price + dessert_price;
-        }
-    }
+function cancelOrder() {
+    const confirmationScreen = document.querySelector(".confirmation-screen");
+    confirmationScreen.classList.remove("activated");
+}
+
+function confirmOrder() {
+    let totalWithComma = total.toFixed(2).toString().replace('.', ',');
+    let msg =
+        `Olá, gostaria de fazer o pedido: %0D%0A- Prato: ${meal} %0D%0A- Bebida: ${drink} %0D%0A- Sobremesa: ${dessert} %0D%0ATotal: R$ ${totalWithComma}%0D%0A%0D%0ANome: ${clientName}%0D%0AEndereço: ${clientAddress}`;
+    window.open("https://wa.me/+5535997518956?text=" + msg);
 }
